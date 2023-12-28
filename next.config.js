a7f486640b298ed8efb5c38d04375a6009b1fe62
Ved Pahune <pahuneved@gmail.com>
2023-12-28 11:51:52 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-
-module.exports = {
-    images: {
-      domains: ['tmdb.org', 'themoviedb.org', 'image.tmdb.org'],
-    },
-  };
