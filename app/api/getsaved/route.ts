191fae9bef4a40e184a2ecf75b9ef180c83fefc8
Ved Pahune <vedpahune@Veds-MacBook-Air.local>
2023-12-29 18:44:03 +0530

    minor bug fix

--- app/api/getsaved/route.ts
+++ app/api/getsaved/route.ts
@@ -2,24 +2,12 @@ import connectDB from "@utils/database";
 import User from "@utils/models/User";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 export const GET = async () => {
-  const { getUser } = getKindeServerSession();
-  const kindeUser = await getUser();
-
+  
   try {
-    if (kindeUser == null) {
-      return new Response(
-        JSON.stringify({ error: "Authentication Required" }),
-        { status: 200 }
-      );
-    }
     await connectDB();
+    const { getUser } = getKindeServerSession();
+    const kindeUser = await getUser();
     const user = await User.findOne({ email: kindeUser.email });
-    if (!user) {
-      // Handle the case where the user is not found in the database
-      return new Response(JSON.stringify({ error: "User not found" }), {
-        status: 404,
-      });
-    }
     const watchlist = user.savedMovies;
     return new Response(JSON.stringify(watchlist), { status: 200 });
   } catch (error) {
