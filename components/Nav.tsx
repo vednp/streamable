0adccc1e8234fc3e852e915f01c1fc3ff82f9483
Ved Pahune <pahuneved@gmail.com>
2024-01-21 00:05:55 +0530

    improved responsive layout and added mobile nav features

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,8 +5,18 @@ import Link from "next/link";
 import {
   RegisterLink,
   LoginLink,
-  LogoutLink
+  LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 const Nav = () => {
@@ -32,28 +42,90 @@ const Nav = () => {
       </Link>
       <div className="text-cyan-100 flex space-x-5 mr-6 cursor-pointer md:hidden">
         <div onClick={toggleMenu}>
-          <svg
-            className="w-6 h-6 cursor-pointer"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
+          {!user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <svg
+                  className="w-6 h-6 cursor-pointer"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <LoginLink>
+                    <strong>Log In</strong>
+                  </LoginLink>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  {" "}
+                  <Link href="/">Home</Link>{" "}
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/movie">Movies</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  {" "}
+                  <Link href="/tv">TV</Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <svg
+                  className="w-6 h-6 cursor-pointer"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href="/watch-later">My Watchlist</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  {" "}
+                  <Link href="/">Home</Link>{" "}
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/movie">Movies</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  {" "}
+                  <Link href="/tv">TV</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  {" "}
+                  <LogoutLink>Log Out</LogoutLink>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
         </div>
       </div>
       {/* Navigation Links */}
-      <div
-        className={`items-center text-cyan-100 mr-16 md:space-x-5 md:flex ${
-          isMenuOpen ? "block" : "hidden"
-        }`}
-      >
+      <div className={`items-center hidden text-cyan-100 mr-16 md:space-x-5 md:flex`}>
         <p className="hover:text-indigo-400 ">
           <Link href="/">Home</Link>
         </p>
@@ -66,16 +138,16 @@ const Nav = () => {
 
         {user ? (
           <>
-          <p className="hover:text-indigo-400 underline">
-          <Link href="/watch-later">My Watchlist</Link>
-        </p>
-           <p className="hover:bg-indigo-400 border-2 border-blue-500 rounded-lg px-3 py-1 ">
-           <LogoutLink>Log Out</LogoutLink>
-         </p>
-         </>
+            <p className="hover:text-indigo-400 underline">
+              <Link href="/watch-later">My Watchlist</Link>
+            </p>
+            <p className="hover:bg-indigo-400 border-2 border-blue-500 rounded-lg px-3 py-1 ">
+              <LogoutLink>Log Out</LogoutLink>
+            </p>
+          </>
         ) : (
           <>
-          <p className="hover:bg-indigo-400 border-2 border-blue-500 rounded-lg px-3 py-1 ">
+            <p className="hover:bg-indigo-400 border-2 border-blue-500 rounded-lg px-3 py-1 ">
               <LoginLink>Log In</LoginLink>
             </p>{" "}
             <p className="hover:bg-indigo-400 border-2 border-blue-500 rounded-lg px-3 py-1 ">
